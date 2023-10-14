e2a8046d0101ae32a80822dd106a771d4e8cb863
FabianStudium <fabian.gazzia@stud.fh-campuswien.ac.at>
2023-10-14 17:15:03 +0200

    input validation and sanitisation

--- main.c
+++ main.c
@@ -18,6 +18,21 @@ int main(const int argc, const char * argv[]) {
         * 1 arg
         * file exists
     */
+    check_argument_count(argc);
+    
+    if(path_is_not_safe(argv[1])) {
+        fprintf(stderr, "Error: Unsafe file path provided. File needs to be in the same directory as the program\n");
+        return EXIT_FAILURE;
+    }
+
+    // Define the size of the destination buffer
+    const size_t buffer_size = 256;
+    char filename[buffer_size];
+
+    if(parse_input_failed(filename, argv[1], buffer_size)) {
+        fprintf(stderr, "Error: Failed to safely copy the input string.\n");
+        return EXIT_FAILURE;
+    }
 
     /*  file operations
         * read content per letter
@@ -44,5 +59,66 @@ int main(const int argc, const char * argv[]) {
    */
     
     return EXIT_SUCCESS;
+}
 
+int check_argument_count(const int argc) {
+    if(argc != 2) {
+        fprintf(stderr, "Please provide a filename.\n\nUsage: ./frequency <path>/<filename>.<extenstion>.");
+        return 0;
+    }
+
+    return 1;
+}
+
+const int file_exists(const char* filename) {
+    FILE* file = fopen(filename, "r");
+
+    if (file == NULL) {
+        fprintf(stderr, "Error: Cannot open file %s\n", filename);
+        return 0; // Does not exist
+    }
+    
+    fclose(file);
+    
+    return 1; // Exists
+}
+
+int parse_input_failed(char* dst, const char* src, const size_t dst_size) {
+    if (src == NULL || dst == NULL) {
+        return 1; // Invalid input
+    }
+
+    // Use strlcpy for safe string copying .. keyword: buffer overflow
+    size_t src_len = strlcpy(dst, src, dst_size);
+
+    // Check if the entire source string was copied
+    if (src_len >= dst_size) {
+        return 1; // Source string was truncated
+    }
+
+    return 0; // Success
+}
+
+/* Source: GPT*/
+/* Input sanitisation .. check if user provides any unsafe characters that could lead to vulnerabilities like path traversal, file access, etc.*/
+int path_is_not_safe(const char* input) {
+    const char* unsafe_char[] = {
+        "..", "/", "\\", 
+        "%2e%2e%2f", "%2e%2e%5c", // url encoded '../' '..\'
+        "%2F", "%5C", // url encoded '/' '\'
+        "%2f", "%5c" // url encoded '/' '\'
+    };
+
+    size_t unsafe_char_count = sizeof(unsafe_char) / sizeof(unsafe_char[0]);
+
+    /*
+    unsafe_char_count calculates the number of elements in the patterns array by dividing the total byte size of the array (sizeof(patterns)) by the byte size of a single element (sizeof(patterns[0])). This ensures that the subsequent loop iterates accurately through each element in the array, preventing out-of-bounds access and making the code adaptable to changes in the array size.
+    */
+
+    for (size_t i = 0; i < unsafe_char_count; i++) {
+        if (strstr(input, unsafe_char[i]) != NULL) {
+            return 1; // Unsafe path
+        }
+    }
+    return 0; // Safe path
 }
--- main.h
+++ main.h
@@ -8,7 +8,13 @@
 #ifndef Header_h
 #define Header_h
 
-#include <stdio.h>
+#include <stdio.h> // for file i/o
 #include <stdlib.h> // for strtol, abs, and EXIT_ constants
+#include <string.h>
+
+int check_argument_count(const int);
+const int file_exists(const char*);
+int parse_input_failed(char*, const char*, const size_t);
+int path_is_not_safe(const char*);
 
 #endif /* Header_h */
