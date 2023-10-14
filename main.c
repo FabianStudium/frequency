bd65d1b7a6df1bd2cf44eb9e1c14cb27d56618db
FabianStudium <fabian.gazzia@stud.fh-campuswien.ac.at>
2023-10-14 13:01:19 +0200

    code plan

--- main.c
+++ main.c
@@ -9,7 +9,40 @@
 
 int main(const int argc, const char * argv[]) {
     
-    printf("Hello world.");
+    /*  variables (sizes)
+        file_stream[]
+        letters[file_stream.length][frequencyInt][frequency%]
+    */
+    
+    /*  input validierung 
+        * 1 arg
+        * file exists
+    */
+
+    /*  file operations
+        * read content per letter
+        * write from stream to 2d array
+            - for i in stream: 
+                for j in string:
+                    string[j] == stream[i] ? string[j][1]++ : string[j+1] = stream[i];
+    */
+
+    /*  sort letter array by frequency
+    */
+
+    /*  frequency
+        * calculate frequency per letter in %
+            - total amount: for i in string: string[i][0] * string[i][1] || string[i][1] + string[i+1][1] ...
+            - frequency per letter: for i in string: string[i][1] / total * 100
+                - maybe store together with string
+                    * string[] -> letter
+                    * string[][] -> frequency in int
+                    * string[][][] -> frequency in %
+    */
+
+   /*   print result
+   */
     
     return EXIT_SUCCESS;
+
 }
